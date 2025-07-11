be29d44774fd59c7defc0b7aabbeff9d44a22753
Gihoon <kih0976@gmail.com>
2025-07-11 17:35:47 +0900

    79강 수강 완료 (사다리, 시네머신 팔로운 카메라 기능 추가)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,21 +7,27 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10.0f;
     [SerializeField] float jumpSpeed = 5.0f;
+    [SerializeField] float climbSpeed = 5.0f;
 
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider;
+    float gravityScaleAtStart;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        gravityScaleAtStart = myRigidbody.gravityScale;
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value)
@@ -31,6 +38,10 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
+        // 플레이어의 Capsule Collider 가 Ground 에 접촉하지 않았다면 함수를 종료한다.
+        // 즉, 바닥에 붙어 있는 상태에서면 점프를 할 수 있다.
+        if (false == myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
+
         if(true == value.isPressed)
         {
             myRigidbody.linearVelocity += new Vector2(0f, jumpSpeed);
@@ -40,7 +51,6 @@ public class PlayerMovement : MonoBehaviour
     void Run()
     {
         Vector2 playerVelocity = new Vector2(runSpeed * moveInput.x, myRigidbody.linearVelocity.y);
-        //myRigidbody.velocity = moveInput;
         myRigidbody.linearVelocity = playerVelocity;
 
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.linearVelocity.x) > Mathf.Epsilon;
@@ -58,5 +68,21 @@ public class PlayerMovement : MonoBehaviour
             // 플레이어의 스케일을 수정하여 이미지를 이동방햐엥 맞게 뒤집는다.
             transform.localScale = new Vector2(Mathf.Sign(myRigidbody.linearVelocity.x), 1f);
         }
+    }
+
+    private void ClimbLadder()
+    {
+        if (false == myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myRigidbody.gravityScale = gravityScaleAtStart;
+            return;
+        }
+
+        Vector2 climbVelocity = new Vector2(myRigidbody.linearVelocity.x, moveInput.y * climbSpeed);
+        myRigidbody.linearVelocity = climbVelocity;
+        myRigidbody.gravityScale = 0;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.linearVelocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
     }
 }
